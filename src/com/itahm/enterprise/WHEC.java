187c5aaf58b327527111c4f4d57cb107e512c029
ITAhM <ITAhM@ITAhM>
2017-07-05 10:34:11 +0900

    ITAhM Agent 1.4

---
+++ src/com/itahm/enterprise/WHEC.java
@@ -0,0 +1,18 @@
+package com.itahm.enterprise;
+
+public class WHEC extends Enterprise {
+
+	public static final String KEY = "A47B055B-55F0-4679-A64D-C8C9726A3DBD";
+	
+	public WHEC() {
+		
+	}
+	
+	public static void main(String [] args) {
+	}
+	
+	@Override
+	public void close() {
+		
+	}
+}
